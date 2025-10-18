fe5d4d0636ddd7704d5b0e78f192aa936413a4df
AntonPetrenko <paa2017sh@gmail.com>
2025-10-18 15:01:38 +0300

    Implement ChirpController and update home view to display chirps

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,13 @@
   {{-- <x-slot name="title">Home</x-slot>// variant one for Set the page title to "Home" --}}
   <x-slot:title>Home</x-slot:title> {{-- variant two for Set the page title to "Home" --}}  
   <div class="max-w-2xl mx-auto">
-            <div class="card bg-base-100 shadow mt-8">
-                <div class="card-body">
-                    <div>
-                        <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
-                        <p class="mt-4 text-base-content/60">This is your brand new Laravel application. Time to make it
-                            sing (or chirp)!</p>
-                    </div>
-                </div>
-            </div>
+    @foreach ($chirps as $chirp)
+      <div class="card bg-base-100 shadow mt-4">
+        <div class="card-body">
+          <h2 class="text-xl font-semibold">{{ $chirp['author'] }}</h2>
+          <p class="mt-2">{{ $chirp['message'] }}</p>
+          <p class="mt-2 text-base-content/60">{{ $chirp['time'] }}</p>
         </div>
+      </div>
+    @endforeach
 </x-layout>
