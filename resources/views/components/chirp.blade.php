335ebe9dc6f6acf9dcc6b2488b67123535768ec9
AntonPetrenko <paa2017sh@gmail.com>
2025-10-30 09:50:17 +0200

    improve styles

--- resources/views/components/chirp.blade.php
+++ resources/views/components/chirp.blade.php
@@ -7,7 +7,7 @@
             @if ($chirp->user)
                 <div class="avatar">
                     <div class="size-10 rounded-full">
-                        <img src="<https://avatars.laravel.cloud/>{{ urlencode($chirp->user->email) }}"
+                        <img src="<https://avatars.laravel.cloud/>{{ urlencode($chirp->user->email) }}?vibe=ocean"
                             alt="{{ $chirp->user->name }}'s avatar" class="rounded-full" />
                     </div>
                 </div>
